b408c0d011b9a3e5714eb0c68b690c484c6c9819
persiansir <48182760+persiansir@users.noreply.github.com>
2024-08-14 14:08:31 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -1,44 +1,47 @@
 <?php
 
-    // CloudFlare IP Ranges
-    // ircf.space
-
-    function getIps($raw) {
-        $ips = [];
-        $fetch = @file_get_contents($raw);
-        if ( isset($fetch) && !empty($fetch) ) {
-            $ips = preg_split("/[\f\r\n]+/", $fetch );
+function getIps($url) {
+    try {
+        $response = file_get_contents($url);
+        if ($response === false) {
+            throw new Exception("Failed to retrieve IP addresses from $url");
         }
-        return $ips;
+        $ips = preg_split("/[\f\r\n]+/", $response);
+        return array_filter($ips, 'strlen');
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage() . "\n";
+        return [];
     }
+}
 
-    $bashsizIps = getIps('https://raw.githubusercontent.com/MortezaBashsiz/CFScanner/main/config/cf.local.iplist');
-    $safariIps = getIps('https://raw.githubusercontent.com/SafaSafari/ss-cloud-scanner/main/ips.txt');
-    $faridIps = getIps('https://raw.githubusercontent.com/vfarid/cf-ip-scanner/main/ipv4.txt');
-    $ircfIps = getIps('https://raw.githubusercontent.com/ircfspace/scanner/main/ipv4.list');
+$bashsizIps = getIps('https://raw.githubusercontent.com/MortezaBashsiz/CFScanner/main/config/cf.local.iplist');
+$safariIps = getIps('https://raw.githubusercontent.com/SafaSafari/ss-cloud-scanner/main/ips.txt');
+$faridIps = getIps('https://raw.githubusercontent.com/vfarid/cf-ip-scanner/main/ipv4.txt');
+$ircfIps = getIps('https://raw.githubusercontent.com/ircfspace/scanner/main/ipv4.list');
 
-    $newList = array_merge($bashsizIps, $safariIps, $faridIps, $ircfIps);
-    $newList = array_filter($newList, 'strlen');
-    $newList = array_unique($newList);
-    natsort($newList);
+$newList = array_merge($bashsizIps, $safariIps, $faridIps, $ircfIps);
+$newList = array_unique($newList);
+natsort($newList);
 
-    $generateList = [];
-    foreach( $newList as $ip ) {
-        if ( empty($ip) ) {
-            continue;
-        }
-        $explode = explode("/", $ip);
-        if ( ! isset($explode[0]) || empty($explode[0]) ) {
-            continue;
-        }
-        $generateList[$explode[0]] = $explode[0].'/24';
+$generateList = [];
+foreach ($newList as $ip) {
+    if (empty($ip)) {
+        continue;
     }
-
-    $export = '';
-    foreach( $generateList as $ip ) {
-        $export .= $ip."\n";
+    $explode = explode("/", $ip);
+    if (!isset($explode[0]) || empty($explode[0])) {
+        continue;
     }
+    $generateList[$explode[0]] = $explode[0] . '/24';
+}
 
+$export = '';
+foreach ($generateList as $ip) {
+    $export .= $ip . "\n";
+}
+
+try {
     file_put_contents("export.ipv4", $export);
-
-?>
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "\n";
+}
